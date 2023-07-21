7c5d27ecde4a0b1de1d7ef0bdad2a5b9c119bc6c
pianti10 <nicolaspiantini@gmail.com>
2023-07-21 01:07:34 -0300

    useMovieDetails

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {Text, View, Image, StyleSheet, Dimensions} from 'react-native';
 import {RootStackParams} from '../navigation/Navigation';
 import {ScrollView} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useMovieDetails} from '../hooks/useMovieDetails';
 
 const screenHeight = Dimensions.get('screen').height;
 
@@ -12,14 +14,21 @@ export const DetailScreen = ({route}: Props) => {
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
+  useMovieDetails(movie.id);
+
   return (
     <ScrollView>
       <View style={styles.imageContainer}>
-        <Image source={{uri}} style={styles.posterImage} />
+        <View style={styles.imageBorder}>
+          <Image source={{uri}} style={styles.posterImage} />
+        </View>
       </View>
       <View style={styles.marginContainer}>
         <Text style={styles.subtitle}>{movie.original_title}</Text>
         <Text style={styles.title}>{movie.title}</Text>
+      </View>
+      <View style={styles.marginContainer}>
+        <Icon name="star-outline" color="grey" size={20} />
       </View>
     </ScrollView>
   );
@@ -27,7 +36,7 @@ export const DetailScreen = ({route}: Props) => {
 
 const styles = StyleSheet.create({
   imageContainer: {
-    overflow: 'hidden',
+    // overflow: 'hidden',
     width: '100%',
     height: screenHeight * 0.7,
     shadowColor: '#000',
@@ -39,7 +48,12 @@ const styles = StyleSheet.create({
     shadowRadius: 6.68,
 
     elevation: 11,
-
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
+  },
+  imageBorder: {
+    flex: 1,
+    overflow: 'hidden',
     borderBottomStartRadius: 25,
     borderBottomEndRadius: 25,
   },
