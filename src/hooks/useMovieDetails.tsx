f9059fba059adba1edd89a5827e1be44ebb2706c
pianti10 <nicolaspiantini@gmail.com>
2023-07-21 01:25:48 -0300

    info actores

--- src/hooks/useMovieDetails.tsx
+++ src/hooks/useMovieDetails.tsx
@@ -1,18 +1,36 @@
 import {useState, useEffect} from 'react';
 import movieDB from '../api/movieDB';
-import { MovieFull } from '../interfaces/movieInterface';
+import {MovieFull} from '../interfaces/movieInterface';
+import {Cast, CreditsResponse} from '../interfaces/creditsInterface';
 
 interface MovieDetails {
   isLoading: boolean;
-  movieFull: MovieFull;
-  cast: any[];
+  movieFull?: MovieFull;
+  cast: Cast[];
 }
 
 export const useMovieDetails = (movieId: number) => {
-  const [state, setState] = useState<MovieDetails>();
+  const [state, setState] = useState<MovieDetails>({
+    isLoading: true,
+    movieFull: undefined,
+    cast: [],
+  });
 
   const getMovieDetails = async () => {
-    const resp = await movieDB.get<MovieFull>(`/${movieId}`);
+    const movieDetailsPromise = await movieDB.get<MovieFull>(`/${movieId}`);
+    const castPromise = await movieDB.get<CreditsResponse>(
+      `/${movieId}/credits`,
+    );
+
+    const [movieDetailsResp, castPromiseResp] = await Promise.all([
+      movieDetailsPromise,
+      castPromise,
+    ]);
+    setState({
+      isLoading: false,
+      movieFull: movieDetailsResp.data,
+      cast: castPromiseResp.data.cast,
+    });
   };
 
   useEffect(() => {
@@ -20,6 +38,6 @@ export const useMovieDetails = (movieId: number) => {
   }, []);
 
   return {
-    state,
+    ...state,
   };
 };
